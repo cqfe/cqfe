fb71123cc0a040216437bdd421767b988421be87
leo <434330692@qq.com>
2024-08-06 08:52:14 +0800

    doc: modify git url

--- packages/generate-api/src/resolve.ts
+++ packages/generate-api/src/resolve.ts
@@ -1,27 +1,28 @@
-import { pick } from 'lodash'
-import { toCamelCase } from './utils'
-import { tmpRequestDocFn, tmpRequestFn } from './templates'
-import { appendFileSync } from 'fs'
+import { pick } from 'lodash';
+import { toCamelCase } from './utils';
+import { tmpRequestDocFn, tmpRequestFn } from './templates';
+import { appendFileSync } from 'fs';
 
 export interface IFieldItem {
-  key: string
-  type: string
-  description: string
-  enum?: string[]
+  key: string;
+  type: string;
+  description: string;
+  enum?: string[];
 }
 
 export interface IResolveRequest {
-  body?: Array<IFieldItem>
-  query?: Array<IFieldItem>
-  path?: Array<IFieldItem>
-  formData?: Array<IFieldItem>
+  body?: Array<IFieldItem>;
+  query?: Array<IFieldItem>;
+  path?: Array<IFieldItem>;
+  formData?: Array<IFieldItem>;
 }
 
-const names = [] as string[]
+const names = [] as string[];
 
 function parseBody(obj: Record<string, any>, parent = 'body') {
-  if (parent.split('.').length > 3) return []
-  const ret: Array<IFieldItem> = []
+  if (parent.split('.').length > 3) return [];
+  const ret: Array<IFieldItem> = [];
+  debugger;
   if (obj.type && typeof obj.type === 'string') {
     switch (obj.type) {
       case 'object':
@@ -32,9 +33,10 @@ function parseBody(obj: Record<string, any>, parent = 'body') {
             description: '',
           },
           ...parseBody(obj.properties, parent),
-        )
-        break
+        );
+        break;
       case 'array':
+        debugger;
         ret.push(
           {
             key: parent,
@@ -42,8 +44,8 @@ function parseBody(obj: Record<string, any>, parent = 'body') {
             description: '',
           },
           ...parseBody(obj.items, `${parent}[]`),
-        )
-        break
+        );
+        break;
       case 'number':
       case 'boolean':
       case 'integer':
@@ -51,15 +53,15 @@ function parseBody(obj: Record<string, any>, parent = 'body') {
         ret.push({
           key: parent,
           ...pick(obj, 'type', 'description'),
-        })
-        break
+        });
+        break;
       default:
-        console.error('has unknown type' + obj.type)
-        break
+        console.error('has unknown type' + obj.type);
+        break;
     }
   } else {
     Object.keys(obj).forEach((key) => {
-      const item = obj[key]
+      const item = obj[key];
       if (item.type === 'object') {
         ret.push(
           {
@@ -68,7 +70,7 @@ function parseBody(obj: Record<string, any>, parent = 'body') {
             description: '',
           },
           ...parseBody(item.properties, `${parent}.${key}`),
-        )
+        );
       } else if (item.type === 'array') {
         ret.push(
           {
@@ -77,16 +79,16 @@ function parseBody(obj: Record<string, any>, parent = 'body') {
             description: '',
           },
           ...parseBody(item.items, `${parent}.${key}[]`),
-        )
+        );
       } else {
         ret.push({
           key: (parent ? `${parent}.` : '') + key,
           ...pick(obj[key], 'type', 'description', 'enum'),
-        })
+        });
       }
-    })
+    });
   }
-  return ret
+  return ret;
 }
 
 // resolve request parameters
@@ -114,66 +116,66 @@ export function resolveRequest(parameters: Array<any>) {
         description: '表单数据',
       },
     ],
-  } as IResolveRequest
+  } as IResolveRequest;
   parameters.forEach((param) => {
     switch (param.in) {
       case 'body':
-        ret.body = [...parseBody(param.schema)]
-        break
+        ret.body = [...parseBody(param.schema)];
+        break;
       case 'path':
         ret.path?.push({
           key: `path.${param.name}`,
           description: param.description,
           type: param.type || param?.schema?.type,
-        })
-        break
+        });
+        break;
       case 'query':
         ret.query?.push({
           key: `query.${param.name}`,
           description: param.description,
           type: param.type || param?.schema?.type,
-        })
-        break
+        });
+        break;
       case 'formData':
         ret.formData?.push({
           key: `formData.${param.name}`,
           description: param.description,
           type: param.type || param?.schema?.type,
-        })
-        break
+        });
+        break;
       default:
-        break
+        break;
     }
-  })
-  ;(Object.keys(ret) as [keyof IResolveRequest]).forEach((key: keyof IResolveRequest) => {
-    if ((ret[key]?.length || 0) <= 1) delete ret[key]
-  })
+  });
+  (Object.keys(ret) as [keyof IResolveRequest]).forEach((key: keyof IResolveRequest) => {
+    if ((ret[key]?.length || 0) <= 1) delete ret[key];
+  });
 
-  return ret
+  return ret;
 }
 
 // generate function name
 export function resolveName(method: string, path: string) {
   // handle when function name is repeated
-  let name = `${method}${toCamelCase(path)}`
-  names.push(name)
-  const nameIndex = names.filter((each) => each === name).length - 1
+  let name = `${method}${toCamelCase(path)}`;
+  names.push(name);
+  const nameIndex = names.filter((each) => each === name).length - 1;
   if (nameIndex > 0) {
-    name = `${name}V${nameIndex}`
+    name = `${name}V${nameIndex}`;
   }
-  return name
+  return name;
 }
 
 // swagger v2 generate request function and write to file
 export function resolvePath(path: string, content: Record<string, any>, outPut: string, requestDoc = true) {
   Object.keys(content).forEach((method) => {
-    const { parameters = [], summary, description } = content[method]
+    const { parameters = [], summary, description } = content[method];
     // 解析请求参数
-    const requestParams = resolveRequest(parameters)
+    const requestParams = resolveRequest(parameters);
     // 解析请求名称
-    const name = resolveName(method, path)
+    const name = resolveName(method, path);
     // 生成jsdoc文档
-    const docs = tmpRequestDocFn(summary || description, requestParams, requestDoc)
+    const docs = tmpRequestDocFn(summary || description, requestParams, requestDoc);
     // 生成函数
     const code = tmpRequestFn(
       { method, url: path, name },
@@ -182,28 +184,28 @@ export function resolvePath(path: string, content: Record<string, any>, outPut: 
       requestParams.query,
       requestParams.body,
       requestParams.formData,
-    )
-    appendFileSync(outPut, code)
-  })
+    );
+    appendFileSync(outPut, code);
+  });
 }
 
 // openapi v3 generate request function and write to file
 export function resolvePathV3(path: string, content: Record<string, any>, outPut: string, requestDoc = true) {
   Object.keys(content).forEach((method) => {
-    const { parameters = [], requestBody, summary, description } = content[method]
+    const { parameters = [], requestBody, summary, description } = content[method];
     if (requestBody) {
       parameters.push({
         in: 'body',
         description: requestBody.description,
         schema: requestBody.content['application/json']?.schema || {},
-      })
+      });
     }
     // resolve request parameters
-    const requestParams = resolveRequest(parameters)
+    const requestParams = resolveRequest(parameters);
     // resolve request name
-    const name = resolveName(method, path)
+    const name = resolveName(method, path);
     // generate jsdoc
-    const docs = tmpRequestDocFn(summary || description, requestParams, requestDoc)
+    const docs = tmpRequestDocFn(summary || description, requestParams, requestDoc);
     // generate function code
     const code = tmpRequestFn(
       { method, url: path, name },
@@ -212,7 +214,7 @@ export function resolvePathV3(path: string, content: Record<string, any>, outPut
       requestParams.query,
       requestParams.body,
       requestParams.formData,
-    )
-    appendFileSync(outPut, code)
-  })
+    );
+    appendFileSync(outPut, code);
+  });
 }
