c92c198f2cb3c0916bad675bf414c5514f0a92fb
leo <leolichuan@qq.com>
2025-07-30 17:49:52 +0800

    feat: init安装依赖时windows报错

--- packages/sdfe/src/actions/init.ts
+++ packages/sdfe/src/actions/init.ts
@@ -25,35 +25,44 @@ export default async function () {
   ])
   if (IS_MULTI_REPO && resTemplate.template === 'subDesktop') {
     // clone 子应用
-    spawnSync('git', ['clone', APP_TEMPLATE_MAP[resTemplate.template].url, resName.name], {
+    const resClone = spawnSync('git', ['clone', APP_TEMPLATE_MAP[resTemplate.template].url, resName.name], {
       cwd: resolve(PROCESS_CWD, MULTI_REPO_DIR),
       stdio: 'inherit',
     })
+    if (resClone.status !== 0) return logger.error(`初始化子应用 ${resName.name} 失败`)
     // 安装依赖
-    spawnSync('pnpm', ['install'], {
+    const resInstall = spawnSync('pnpm', ['install'], {
       stdio: 'inherit',
+      shell: true,
     })
+    if (resInstall.status !== 0) logger.error('安装依赖失败，请手动安装 pnpm install')
     // 删除 .git
-    spawnSync('rm', ['-rf', '.git'], {
+    const resRm = spawnSync('rm', ['-rf', '.git'], {
       cwd: resolve(PROCESS_CWD, MULTI_REPO_DIR, resName.name),
       stdio: 'inherit',
     })
+    if (resRm.status !== 0)
+      logger.error(`删除.git 失败，请手动删除 cd ./${MULTI_REPO_DIR}/${resName.name} && rm -rf .git`)
     logger.success(`初始化子应用 ${MULTI_REPO_DIR}/${resName.name} 成功`)
   } else {
     // clone 项目
-    spawnSync('git', ['clone', APP_TEMPLATE_MAP[resTemplate.template].url, resName.name], {
+    const resClone = spawnSync('git', ['clone', APP_TEMPLATE_MAP[resTemplate.template].url, resName.name], {
       stdio: 'inherit',
     })
+    if (resClone.status !== 0) return logger.error(`初始化子应用 ${resName.name} 失败`)
     // 安装依赖
-    spawnSync('pnpm', ['install'], {
+    const resInstall = spawnSync('pnpm', ['install'], {
       cwd: resolve(PROCESS_CWD, resName.name),
       stdio: 'inherit',
+      shell: true,
     })
+    if (resInstall.status !== 0) logger.error(`安装依赖失败，请手动安装 cd ./${resName.name} && pnpm install`)
     // 删除 .git
-    spawnSync('rm', ['-rf', '.git'], {
+    const resRm = spawnSync('rm', ['-rf', '.git'], {
       cwd: resolve(PROCESS_CWD, resName.name),
       stdio: 'inherit',
     })
+    if (resRm.status !== 0) logger.error(`删除.git 失败，请手动删除 cd ./${resName.name} && rm -rf .git`)
     logger.success(`初始化项目 ${resName.name} 成功`)
   }
 }
